60dec3cca788078c5bf54948b81730984d30aa7e
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-01 15:20:55 +0100

    init

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
 <!-- Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.-->
+<?php 
+// creo il mio database
+$faqs = [
+  'Come state implementando la recente decisione della Corte di giustizia della Unione europea (CGUE) relativa al diritto all oblio?' => 
+  [
+    'p' => 'La recente decisione della Corte di giustizia della Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.',
+    'p' =>'Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
+
+    ',
+    'p' =>'',
+    'p' =>'',
+    'p' =>'',
+  ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quae explicabo fugit, voluptas quia ab cupiditate doloribus fugiat veritatis vitae, mollitia deserunt eum sed ipsum aliquid libero sequi sunt totam!
+  </header>
+  <main>
+  </main>
+  <footer>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum enim doloribus quaerat quo optio corporis, quod aliquam sint dolorem laborum iste, dicta iusto a dolor accusamus id minus veritatis modi.
+  </footer>
+</body>
+</html>
